619d4f8e2b1923abd44910c0939b19c0719480a2
Koray Oksay <koray.oksay@gmail.com>
2024-08-19 14:54:01 +0000

    fix cni deploy script

--- labs/01_magicless-kubernetes/250_worker_cni.sh
+++ labs/01_magicless-kubernetes/250_worker_cni.sh
@@ -19,5 +19,5 @@ rm -f cni-plugins-linux-amd64-vv${CNI_PLUGINS_VERSION}.tgz
 export POD_CIDR="192.168.1$(hostname | tail -c 2).0/24"
 envsubst < 10-bridge.conf > 10-bridge.conf.subst
 
-install -o root -m 0644 10-bridge.conf /etc/cni/net.d/10-bridge.conf
+install -o root -m 0644 10-bridge.conf.subst /etc/cni/net.d/10-bridge.conf
 install -o root -m 0644 99-loopback.conf /etc/cni/net.d/99-loopback.conf
